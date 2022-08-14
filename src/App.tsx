c8a12c9d0e3a6e83413bfe1b34bdb5b830bece02
Samuel Majok <82509653+SamiiE99@users.noreply.github.com>
2022-08-14 14:18:44 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import QuestionCard from "./components/QuestionCard";
+
 
 function App() {
+
+  const startTrivia = async() => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score: 0</p>
+      <p className="question">Loading Questions...</p>
+      <QuestionCard/>
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
