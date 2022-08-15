b9f1da4c4340c976ceae8d636954637ef180344e
Samuel Majok <82509653+SamiiE99@users.noreply.github.com>
2022-08-15 15:29:59 +0200

    another commit

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -22,5 +22,31 @@ export const GlobalStyle = createGlobalStyle`
     font-family: "Quicksand";
     color: white;
   }
+`
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+
+  h1{
+    font-family: Quicksand;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-size: 100%;
+    background-clip: text;
+    --webkit-background-clip: text;
+    --webkit-text-fill-color: transparent;
+    --moz-background-clip: text;
+    --moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+  }
 `
